60b12e3485df2efcb4b235489499a82daaf40244
ny1111111 <2875615943@qq.com>
2021-04-22 20:32:09 +0800

    Added a new  program py

---
+++ 3.py
@@ -0,0 +1,9 @@
+print("------game boy------")
+temp = input("hellow, please give me a digit:")
+guess=int(temp)
+if guess==8:
+    print("yes\ngood")
+    
+else:
+    print("oh you are ")
+    print("game over")
